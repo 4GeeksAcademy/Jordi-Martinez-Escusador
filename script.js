37a8b84722144889304a678739e22c31ce2312ee
Jordi Guiusseppe Martinez Valencia <166647357+JordiBeppe@users.noreply.github.com>
2024-06-03 18:12:43 +0000

    Added index, script & styles

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    let array1 = ["Mi perro", "Un gato", "Mi tío", "Un vecino", "Elprimo de un amigo"]; //quien
+    let array1 = ["Mi perro", "Un gato", "Mi tío", "Un vecino", "El primo de un amigo"]; //quien
     let array2 = ["se comío", "aventó", "empeñó", "se llevó", "me hizo"]; //que hizo
     let array3 = ["mi tarea", "el reporte", "mis llaves", "el carro", "la receta"]; //a que lo hizo
     let array4 = ["a noche", "ayer", "la semana pasada", "en la mañana", "ahorita"]; //cuando
